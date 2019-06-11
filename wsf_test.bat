1083e6b72df16403058e170cf7d0631409ec7d82
delta-12 <40147170+delta-12@users.noreply.github.com>
2019-06-11 09:59:17 -0400

    Add files via upload

---
+++ wsf_test.bat
@@ -0,0 +1,15 @@
+@echo off
+echo batch output
+cscript //nologo "%~f0?.wsf" //job:VBS
+pause
+exit /b
+
+
+<package>
+  <job id="VBS">
+    <script language="VBScript">
+      WScript.Echo "VBScript output called by batch"
+      call jsEcho
+    </script>
+  </job>
+</package>
